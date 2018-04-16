31acb3ded0c596e02c05a6aae210c14d51d7772d
Kartikya Thakur <kartikya.thakur@cygrp.com>
2018-04-15 22:39:09 -0500

    Initial commit

---
+++ JsonToXml/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using Newtonsoft.Json;
+using JsonToXml.Utilities;
+
+namespace JsonToXml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2 || args[0] == "-help" || args[0] == "-h")
+            {
+                Console.WriteLine("\n\rUtility to convert JSON file to XML\n\rUsage:\n\r\tJsonToXml.exe <inputFilePath> <outputFilePath>");
+                return;
+            }
+            if (!File.Exists(args[0]))
+            {
+                Console.WriteLine("\n\rInput file not found!");
+                return;
+            }
+            string jsonData = File.ReadAllText(args[0]);
+            XmlDocument doc = JsonConvert.DeserializeXmlNode(jsonData, "root");
+            File.WriteAllText(args[1], doc.Stringify());
+        }
+    }
+}
---
+++ JsonToXml/Utilities/XmlHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace JsonToXml.Utilities
+{
+    public static class XmlHelper
+    {
+        public static string Stringify(this XmlDocument xmlDocument)
+        {
+            using (var stringWriter = new StringWriter())
+            using (var xmlTextWriter = XmlWriter.Create(stringWriter))
+            {
+                xmlDocument.WriteTo(xmlTextWriter);
+                xmlTextWriter.Flush();
+                return stringWriter.GetStringBuilder().ToString();
+            }
+        }
+    }
+}
